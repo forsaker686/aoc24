b566ecf2689814c9c8846d43341021d3d5e14a89
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2024-12-04 10:34:52 +0100

    day 4 - part 1 solution

---
+++ day4.php
@@ -0,0 +1,98 @@
+<?php
+$vnos = fopen("day4.txt", "r") or die("Unable to open file!");
+$podatki = fread($vnos,filesize("day4.txt"));
+fclose($vnos);
+$razbito = preg_split("/\n/", $podatki);
+$skupaj = 0;
+for($i = 0; $i < count($razbito); $i++) {
+	for($j=0;$j < strlen($razbito[$i]); $j++) {
+			if($razbito[$i][$j] == "X") {
+				$prostorLevo = $j > 2;
+				$prostorDesno = $j < strlen($razbito[$i]) -3;
+				$prostorGor = $i > 2;
+				$prostorDol = $i < count($razbito) -3;
+				if($prostorLevo) {
+					if($razbito[$i][$j-1] == "M"){
+						if($razbito[$i][$j-2] == "A"){
+							if($razbito[$i][$j-3] == "S"){
+								$skupaj++;
+							}
+						}
+					}
+
+					//diagonala gor
+					if($prostorGor) {
+						if($razbito[$i-1][$j-1] == "M"){
+							if($razbito[$i-2][$j-2] == "A"){
+								if($razbito[$i-3][$j-3] == "S"){
+									$skupaj++;
+								}
+							}
+						}
+					}
+					//diagonala dol
+					if($prostorDol) {
+						if($razbito[$i+1][$j-1] == "M"){
+							if($razbito[$i+2][$j-2] == "A"){
+								if($razbito[$i+3][$j-3] == "S"){
+									$skupaj++;
+								}
+							}
+						}
+					}
+				}
+				//desno
+				if($prostorDesno) {
+					if($razbito[$i][$j+1] == "M"){
+						if($razbito[$i][$j+2] == "A"){
+							if($razbito[$i][$j+3] == "S"){
+								$skupaj++;
+							}
+						}
+					}
+					//diagonala gor
+					if($prostorGor) {
+						if($razbito[$i-1][$j+1] == "M"){
+							if($razbito[$i-2][$j+2] == "A"){
+								if($razbito[$i-3][$j+3] == "S"){
+									$skupaj++;
+								}
+							}
+						}
+					}
+					//diagonala dol
+					if($prostorDol) {
+						if($razbito[$i+1][$j+1] == "M"){
+							if($razbito[$i+2][$j+2] == "A"){
+								if($razbito[$i+3][$j+3] == "S"){
+									$skupaj++;
+								}
+							}
+						}
+					}
+				}
+
+				if($prostorDol) {
+					if($razbito[$i+1][$j] == "M"){
+						if($razbito[$i+2][$j] == "A"){
+							if($razbito[$i+3][$j] == "S"){
+								$skupaj++;
+							}
+						}
+					}
+				}
+				if($prostorGor) {
+					if($razbito[$i-1][$j] == "M"){
+						if($razbito[$i-2][$j] == "A"){
+							if($razbito[$i-3][$j] == "S"){
+								$skupaj++;
+							}
+						}
+					}
+				}
+			}
+	}
+}
+echo 'part 1: '.$skupaj;
+
+?>
