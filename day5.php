d3f58162157e0cd20e2f1e86962186f35b0e969e
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2024-12-05 14:37:54 +0100

    day5 - part2 with overlooping

--- day5.php
+++ day5.php
@@ -1,6 +1,6 @@
 <?php
-$vnos = fopen("day5.txt", "r") or die("Unable to open file!");
-$podatki = fread($vnos,filesize("day5.txt"));
+$vnos = fopen("day5_e.txt", "r") or die("Unable to open file!");
+$podatki = fread($vnos,filesize("day5_e.txt"));
 fclose($vnos);
 $razbito = preg_split("/\n/", $podatki);
 $varne = array();
@@ -20,31 +20,54 @@ foreach($razbito as $razbit) {
 }
 //part 1
 $skupaj = 0;
+$skupaj2 = 0;
 $popravljene = array();
-for($i=0;$i < count($por); $i++) {
-	$stevilka1 = substr($por[$i], 0, 2);
-	$stevilka2 = substr($por[$i], 3,2);
-	for($j = 0; $j < count($pn); $j++) {
-		if(str_contains($pn[$j], $stevilka1) && str_contains($pn[$j],$stevilka2)) {
-			$posamezna = explode(",", $pn[$j]);
-			$poz1 = 0;
-			$poz2 = 0;
-			for($k = 0; $k < count($posamezna); $k++) {
-				if($posamezna[$k] == $stevilka1) {
-					$poz1 = $k;
-				}else if($posamezna[$k] == $stevilka2){
-					$poz2 = $k;
+$st_loopov = 0;
+while($st_loopov <=3) {
+	for($i=0;$i < count($por); $i++) {
+		$stevilka1 = substr($por[$i], 0, 2);
+		$stevilka2 = substr($por[$i], 3,2);
+		for($j = 0; $j < count($pn); $j++) {
+			if(str_contains($pn[$j], $stevilka1) && str_contains($pn[$j],$stevilka2)) {
+				$posamezna = explode(",", $pn[$j]);
+				$poz1 = 0;
+				$poz2 = 0;
+				for($k = 0; $k < count($posamezna); $k++) {
+					if($posamezna[$k] == $stevilka1) {
+						$poz1 = $k;
+					}
+					if($posamezna[$k] == $stevilka2){
+						$poz2 = $k;
+					}
 				}
-			}
-			if($poz2 < $poz1) {
-				$zacasna = $posamezna[$poz2];
-				$posamezna[$poz2] = $posamezna[$poz1];
-				$posamezna[$poz1] = $zacasna;
-				$pn[$j] = implode(",", $posamezna);
-				$popravljene[] = $j;
+				if($stevilka1 == '75' && $stevilka2 == '47') {
+					var_dump('pozicija:'.$i.' - '.$pn[$j]);
+					echo '<br/>';
+					var_dump($stevilka1.':'.$poz1.' '.$stevilka2.':'.$poz2.' '.($poz2 < $poz1));
+					echo '<br/>';
+				}
+				if($poz2 < $poz1) {
+					// var_dump($pn[$j]);
+					// echo '<br/>';
+					// var_dump($posamezna[$poz1].' '.$posamezna[$poz2]);
+					// echo '<br/>';
+					// var_dump($pn[$j]);
+					$zacasna = $posamezna[$poz2];
+					$posamezna[$poz2] = $posamezna[$poz1];
+					$posamezna[$poz1] = $zacasna;
+					$pn[$j] = implode(",", $posamezna);
+					$popravljene[] = $j;
+					// echo '<pre>';
+					// var_dump($pn[$j]);
+					// echo '</pre>';
+					// $sredinska = round(count($posamezna)/2, 0, PHP_ROUND_HALF_DOWN);
+					// $sredinska = $posamezna[$sredinska];
+					// $skupaj2+=$sredinska;
+				}
 			}
 		}
 	}
+	$st_loopov++;
 }
 for($i=0;$i < count($pn); $i++) {
 	if(!in_array($i, $popravljene)) {
@@ -54,7 +77,21 @@ for($i=0;$i < count($pn); $i++) {
 		$skupaj+=$sredinska;
 	}
 }
-echo 'part 1:'.$skupaj;
+$popravljene = array_values(array_unique($popravljene));
+for($i=0;$i< count($pn); $i++) {
+	if(in_array($i, $popravljene)) {
+				// echo '<pre>';
+				// var_dump($pn[$i]);
+				// echo '</pre>';
+		$posamezna = explode(",", $pn[$i]);
+		$sredinska = round(count($posamezna)/2, 0, PHP_ROUND_HALF_DOWN);
+		$sredinska = $posamezna[$sredinska];
+		$skupaj2+=$sredinska;		
+	}
+}
 
+//part 1:5329part 2:5833  - OK
+echo 'part 1:'.$skupaj;
+echo 'part 2:'.$skupaj2;
 
 ?>
