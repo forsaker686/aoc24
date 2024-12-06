fde7ecba48768b4a117caab7531a49bd5d2f2367
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2024-12-06 20:34:31 +0100

    day 6 - part 2
    not optimal code, but it works

--- day6.php
+++ day6.php
@@ -75,7 +75,7 @@ while($isci) {
 		}
 	}
 }
-
+//part 1
 $mapa = implode("\n", $razbito);
 //map drawing
 echo '<pre>';
@@ -83,5 +83,74 @@ echo $mapa;
 echo '</pre>';
 preg_match_all("/X/", $mapa, $izpis);
 echo 'part1: '.count($izpis[0])-1;
+
+//part 2 - still not optimal - will improve later
+function ovire($mapa, $zX, $zY, $x, $y, $pot) {
+	$pot = "gor";
+	$mapa[$x][$y] = "#";
+	$x = $zX;
+	$y = $zY;
+	$obiskane = array();
+	$loop = false;
+	while(true) {
+		$koordinate = "{$x},{$y},{$pot}";
+		if(array_search($koordinate, $obiskane)) {
+			$izris = implode("\n", $mapa);
+			$loop = true;
+			break;
+		}
+		$obiskane[] = $koordinate;
+		if($x -1 < 0 || $x + 1 == count($mapa) || $y - 1 < 0 || $y + 1 == count($mapa)) {
+			break;
+		}
+		if($pot == "gor") {
+			if($mapa[$x-1][$y] !== "#") {
+				$x-= 1;
+			}else if($mapa[$x-1][$y] == "#") {
+				$pot = "desno";
+				$y+= 1;
+			}
+		}
+		if($pot == "desno") {
+			if($mapa[$x][$y+1] !== "#") {
+				$y+= 1;
+			} else if($mapa[$x][$y+1] == "#") {
+				$x+= 1;
+				$pot = "dol";
+			}
+		}
+		if($pot == "dol") {
+			if($mapa[$x+1][$y] !== "#") {
+				$x+=1;
+			}else if($mapa[$x+1][$y] == "#") {
+				$pot = "levo";
+				$y-= 1;
+			}
+		}
+		if($pot == "levo") {
+			if($mapa[$x][$y-1] !== "#") {
+				$y-= 1;
+			} else if($mapa[$x][$y-1] == "#") {
+				$x-= 1;
+				$pot = "gor";
+			}
+		}
+	}
+	return $loop;
+}
+$ovir = 0;
+$x = $zacetek[0];
+$y = $zacetek[1];
+foreach($path as $posamezna) {
+	$cords = explode(",", $posamezna);
+	$zX = $cords[0];
+	$zY = $cords[1];
+	$pot = $cords[2];
+	if(ovire($zemljevid, $x, $y, $zX, $zY, $pot)) {
+		$ovir++;
+	}
+}
+echo '<br/>';
+echo 'part2: '.$ovir-1;
 
 ?>
