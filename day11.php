15185e806e38108d708154fa44bac66cc287373c
forsaker686 <95349429+forsaker686@users.noreply.github.com>
2024-12-11 15:55:54 +0100

    day11 - part 2

--- day11.php
+++ day11.php
@@ -28,4 +28,72 @@ while($stevec < 25) {
 }
 
 echo 'part 1:'.count($kamni);
+
+//part2
+
+function nastavi_array($podatki) {
+	$kamni = array();
+	$pKamni = explode(" ", $podatki);
+	foreach($pKamni as $kamen) {
+		if(isset($kamni[$kamen]) == FALSE) {
+			$kamni[$kamen]=0;
+		}
+		$kamni[$kamen]++;
+	}
+	return $kamni;
+}
+
+function utripaj($rocks) {
+	$novi = array();
+	foreach($rocks as $vrednost => $kamen) {
+		$dolzina = strlen($vrednost);
+		$pol = intdiv($dolzina,2);
+		if($vrednost == 0) {
+			// $nov = 1;
+			// $novi[] = 1;
+			if(isset($novi['1']) == FALSE) {
+				$novi['1'] =0;
+			}
+			$novi['1'] +=$kamen;
+		}else{ 
+			if($dolzina==(pol*2)) {
+				$polovica = strlen($kamen)/2;
+				$prvi = substr($vrednost, 0, $pol);
+				$drugi = substr($vrednost, $pol, $pol);
+				$drugi = ltrim($drugi,'0');
+				if ($drugi=='') $drugi='0';
+				if(isset($novi[$prvi]) == FALSE) {
+					$novi[$prvi] = 0;
+				}
+				if(isset($novi[$drugi]) == FALSE) {
+					$novi[$drugi] = 0;
+				}
+				$novi[$prvi]+=$kamen;
+				$novi[$drugi]+=$kamen;
+			}else {
+				$nov = $kamen*2024;
+				$nov = bcmul($vrednost, '2024');
+				if(isset($novi[$nov]) == FALSE) {
+					$novi[$nov] = 0;
+				}
+				// $novi[] = $nov;
+				$novi[$nov] += $kamen;
+			}
+		}
+	}
+	return $novi;
+}
+
+$kamni = nastavi_array($podatki);
+
+for($i=0; $i < 75; $i++) {
+	$kamni = utripaj($kamni);
+	$skupaj = 0;
+	foreach($kamni as $j => $kamen) {
+		$skupaj = bcadd($skupaj, $kamen);
+	}
+	if($i==74) {
+		echo 'part 2:'.$skupaj;
+	}
+}
 ?>
